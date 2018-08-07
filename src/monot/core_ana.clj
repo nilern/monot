e327360a6c2155a886cf6d356ed9dd7e312c0b93
Pauli Jaakkola <pauli.jaakkola@metosin.fi>
2018-08-07 16:51:10 +0300

    Bug fixing, reordering declarations.

--- src/monot/core_ana.clj
+++ src/monot/core_ana.clj
@@ -96,109 +96,6 @@
       (:const :local :quote :the-var) true
       false)))
 
-(defprotocol ContEmitter
-  (continue-expr [self expr])
-  (continue-computation [self computation]))
-
-(declare ->NodeCont convert continue trivializing emit-bind)
-
-(deftype NodeCont [parent    ; The parent continuation of this
-                   orig-node ; The unconverted AST node
-                   new-node  ; The (partial) converted AST node
-                   path]      ; Path to the subnode whose conversion this is waiting for
-  IsTrivial
-  (trivial? [_] false)
-
-  ContEmitter
-  (continue-expr [_ expr]
-    (trivializing expr (fn [aexpr]
-                         (let [new-node (assoc-in new-node path aexpr)]
-                           (if-let [[subnode path*] (next-path orig-node path)]
-                             (convert (->NodeCont parent orig-node new-node path*) subnode)
-                             (continue parent new-node))))))
-
-  (continue-computation [_ computation]
-    (emit-bind computation
-      (fn [acomp]
-        (let [new-node (assoc-in new-node path acomp)]
-          (if-let [[subnode path*] (next-path orig-node path)]
-            (convert (->NodeCont parent orig-node new-node path*) subnode)
-            (continue parent new-node)))))))
-
-(deftype BindCont [parent]
-  IsTrivial
-  (trivial? [_] false)
-
-  ContEmitter
-  (continue-expr [_ expr] (continue-computation parent expr))
-  (continue-computation [_ computation] (continue-computation parent computation)))
-
-(deftype NamedCont [cont-ref]
-  IsTrivial
-  (trivial? [_] true)
-
-  ContEmitter
-  (continue-expr [_ expr] (assert false "unimplemented"))
-  (continue-computation [_ computation] (assert false "unimplemented")))
-
-(deftype TailCont [pure-ref]
-  IsTrivial
-  (trivial? [_] true)
-
-  ContEmitter
-  (continue-expr [_ expr] {:op       :invoke
-                           :fn       pure-ref
-                           :args     [expr]
-                           :children [:fn :args]})
-  (continue-computation [_ computation] computation))
-
-(defn- continue [cont ast]
-  (if (monadic? ast)
-    (continue-computation cont ast)
-    (continue-expr cont ast)))
-
-(defmulti convert-monadic (fn [_cont ast] (:op ast)))
-
-(defmethod convert-monadic :host-interop [cont {:keys [target] :as ast}]
-  (convert (->NodeCont cont ast ast [:target]) target))
-
-(defmethod convert-monadic :instance? [cont {:keys [target] :as ast}]
-  (convert (->NodeCont cont ast ast [:target]) target))
-
-(defmethod convert-monadic :instance-call [cont {:keys [instance] :as ast}]
-  (convert (->NodeCont cont ast ast [:instance]) instance))
-
-(defmethod convert-monadic :instance-field [cont {:keys [instance] :as ast}]
-  (convert (->NodeCont cont ast ast [:instance]) instance))
-
-(defmethod convert-monadic :invoke [cont {f :fn [computation] :args :as ast}]
-  (if (!-node? ast)
-    (convert (->BindCont cont) computation)
-    (convert (->NodeCont cont ast ast [:fn]) f)))
-
-(defmethod convert-monadic :primitive-invoke [cont {f :fn [computation] :args :as ast}]
-  (if (!-node? ast)
-    (convert (->BindCont cont) computation)
-    (convert (->NodeCont cont ast ast [:fn]) f)))
-
-(defmethod convert-monadic :protocol-invoke [cont {f :protocol-fn [computation] :args :as ast}]
-  (if (!-node? ast)
-    (convert (->BindCont cont) computation)
-    (convert (->NodeCont cont ast ast [:protocol-fn]) f)))
-
-(defmethod convert-monadic :static-call [cont {[arg] :args :as ast}]
-  (convert (->NodeCont cont ast ast [:args 0]) arg))
-
-(defmethod convert-monadic :vector [cont {[item :as items] :items :as ast}]
-  (if (seq items)
-    (convert (->NodeCont cont ast ast [:items 0]) item)
-    (continue cont ast)))
-
-(defn- convert [cont ast]
-  (if (monadic? ast)
-    (convert-monadic cont ast)
-    (continue-expr cont ast)))
-
 (defn- trivializing [expr f]
   (if (trivial? expr)
     (f expr)
@@ -233,35 +130,133 @@
                :variadic?   false
                :atom        aatom}
         recur-label (gensym 'recur)]
-    {:op :invoke
-     :fn {:op :var
-          :form `flat-map
-          :var #'flat-map}
-     :args [computation
-            {:op :fn
-             :variadic? false
-             :max-fixed-arity 1
-             :methods [{:op :fn-method
-                        :loop-id recur-label
-                        :variadic? false
-                        :params [{:op :binding
-                                  :form name
-                                  :name name
-                                  :local :arg
-                                  :arg-id 0
-                                  :variadic? false
-                                  :atom aatom
-                                  :children []}]
-                        :fixed-arity 1
-                        :body {:op :do
-                               :statements []
-                               :ret (f acomp)
-                               :body? true
-                               :children [:statements :ret]}
-                        :children [:params :body]}]
-             :once false
-             :children [:methods]}]
+    {:op       :invoke
+     :fn       {:op   :var
+                :form `flat-map
+                :var  #'flat-map}
+     :args     [computation
+                {:op              :fn
+                 :variadic?       false
+                 :max-fixed-arity 1
+                 :methods         [{:op          :fn-method
+                                    :loop-id     recur-label
+                                    :variadic?   false
+                                    :params      [{:op        :binding
+                                                   :form      name
+                                                   :name      name
+                                                   :local     :arg
+                                                   :arg-id    0
+                                                   :variadic? false
+                                                   :atom      aatom
+                                                   :children  []}]
+                                    :fixed-arity 1
+                                    :body        {:op         :do
+                                                  :statements []
+                                                  :ret        (f acomp)
+                                                  :body?      true
+                                                  :children   [:statements :ret]}
+                                    :children    [:params :body]}]
+                 :once            false
+                 :children        [:methods]}]
      :children [:fn :args]}))
+
+(defprotocol ContEmitter
+  (continue [self expr])
+  (continue-computation [self computation]))
+
+(declare ->NodeCont convert)
+
+(deftype NodeCont [parent                                   ; The parent continuation of this
+                   orig-node                                ; The unconverted AST node
+                   new-node                                 ; The (partial) converted AST node
+                   path]                                    ; Path to the subnode whose conversion this is waiting for
+  IsTrivial
+  (trivial? [_] false)
+
+  ContEmitter
+  (continue [_ expr]
+    (trivializing expr (fn [aexpr]
+                         (let [new-node (assoc-in new-node path aexpr)]
+                           (if-let [[subnode path*] (next-path orig-node path)]
+                             (convert (->NodeCont parent orig-node new-node path*) subnode)
+                             (continue parent new-node))))))
+
+  (continue-computation [_ computation]
+    (emit-bind computation
+               (fn [acomp]
+                 (let [new-node (assoc-in new-node path acomp)]
+                   (if-let [[subnode path*] (next-path orig-node path)]
+                     (convert (->NodeCont parent orig-node new-node path*) subnode)
+                     (continue parent new-node)))))))
+
+(deftype BindCont [parent]
+  IsTrivial
+  (trivial? [_] false)
+
+  ContEmitter
+  (continue [self expr] (continue-computation self expr))
+  (continue-computation [_ computation]
+    (assert (!-node? computation))
+    (continue-computation parent (get-in computation [:args 0]))))
+
+(deftype NamedCont [cont-ref]
+  IsTrivial
+  (trivial? [_] true)
+
+  ContEmitter
+  (continue [_ expr] (assert false "unimplemented"))
+  (continue-computation [_ computation] (assert false "unimplemented")))
+
+(deftype TailCont [pure-ref]
+  IsTrivial
+  (trivial? [_] true)
+
+  ContEmitter
+  (continue [_ expr] {:op       :invoke
+                      :fn       pure-ref
+                      :args     [expr]
+                      :children [:fn :args]})
+  (continue-computation [_ computation] computation))
+
+(defmulti convert-monadic (fn [_cont ast] (:op ast)))
+
+(defmethod convert-monadic :host-interop [cont {:keys [target] :as ast}]
+  (convert (->NodeCont cont ast ast [:target]) target))
+
+(defmethod convert-monadic :instance? [cont {:keys [target] :as ast}]
+  (convert (->NodeCont cont ast ast [:target]) target))
+
+(defmethod convert-monadic :instance-call [cont {:keys [instance] :as ast}]
+  (convert (->NodeCont cont ast ast [:instance]) instance))
+
+(defmethod convert-monadic :instance-field [cont {:keys [instance] :as ast}]
+  (convert (->NodeCont cont ast ast [:instance]) instance))
+
+(defmethod convert-monadic :invoke [cont {f :fn :as ast}]
+  (if (!-node? ast)
+    (let [[arg arg-path] (next-path ast [:fn])]             ; Skip `f` to avoid generating any references to `!`.
+      ;; Use ->BindCont to redirect `continue` calls to `continue-computation`:
+      (convert (->NodeCont (->BindCont cont) ast ast arg-path) arg))
+    (convert (->NodeCont cont ast ast [:fn]) f)))
+
+(defmethod convert-monadic :primitive-invoke [cont {f :fn :as ast}]
+  (convert (->NodeCont cont ast ast [:fn]) f))
+
+(defmethod convert-monadic :protocol-invoke [cont {f :protocol-fn :as ast}]
+  (convert (->NodeCont cont ast ast [:protocol-fn]) f))
+
+(defmethod convert-monadic :static-call [cont {[arg] :args :as ast}]
+  (convert (->NodeCont cont ast ast [:args 0]) arg))
+
+(defmethod convert-monadic :vector [cont {[item :as items] :items :as ast}]
+  (if (seq items)
+    (convert (->NodeCont cont ast ast [:items 0]) item)
+    (continue cont ast)))
+
+(defn- convert [cont ast]
+  (if (monadic? ast)
+    (convert-monadic cont ast)
+    (continue cont ast)))
 
 ;;;; Syntax Extensions
 ;;;; ===============================================================================================
@@ -278,11 +273,11 @@
         let-locals (into {} (map (fn [[name _]] [name {:op :local, :form name, :name name}])) &env)
         body-locals (assoc let-locals pure-name pure-ref)]
     (emit-form {:op       :let
-                :bindings [{:op    :binding
-                            :form  pure-name
-                            :name  pure-name
-                            :local :let
-                            :init  (ana/analyze pure (assoc (ana/empty-env) :locals let-locals))
+                :bindings [{:op       :binding
+                            :form     pure-name
+                            :name     pure-name
+                            :local    :let
+                            :init     (ana/analyze pure (assoc (ana/empty-env) :locals let-locals))
                             :children []}]
                 :body     (->> (ana/analyze `(do ~@body) (assoc (ana/empty-env) :locals body-locals))
                                annotate-effects
