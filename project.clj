a8b18af5c842e060e7490c0003a1dd4725c41493
Pauli Jaakkola <pauli.jaakkola@metosin.fi>
2018-08-10 15:55:37 +0300

    Update tools.analyser.jvm dependency.

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.analyzer.jvm "0.7.0"]])
+                 [org.clojure/tools.analyzer.jvm "0.7.2"]])
